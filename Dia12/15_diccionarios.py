4ae5daa1a897c6b26eb5571439c5db8907ef0466
Emmanuel Nieto M <emmanuel.nieto.munoz@ciisa.cl>
2024-02-20 15:36:22 -0300

    se estructura laboratorio day 12 de Python

--- Dia12/15_diccionarios.py
+++ Dia12/15_diccionarios.py
@@ -1,18 +1,21 @@
-# Definición de un diccionario de estudiantes
+"""
+Definición de un diccionario de estudiantes
+Acceso e impresión de los datos del diccionario
+Agregar un nuevo estudiante al diccionario
+Eliminar un estudiante del diccionario
+"""
+
 estudiantes = {
     "Juan": 18,
     "María": 20,
     "Carlos": 19
 }
 
-# Acceso e impresión de los datos del diccionario
 print("Edad de Juan:", estudiantes["Juan"])
 print("Edad de María:", estudiantes.get("María"))
 
-# Agregar un nuevo estudiante al diccionario
 estudiantes["Ana"] = 21
 print("Diccionario después de agregar a Ana:", estudiantes)
 
-# Eliminar un estudiante del diccionario
 del estudiantes["Carlos"]
 print("Diccionario después de eliminar a Carlos:", estudiantes)
---
+++ Dia12/repaso.py
@@ -0,0 +1,17 @@
+"""
+Intentar modificar el primer elemento del conjunto es inválido y generará un error.
+Por lo tanto, la línea a continuación está comentada para evitar un error de asignación.
+equipaje[0] = "abrigo"
+Accedemos al primer elemento del conjunto usando el índice 0 y lo imprimimos.
+"""
+
+equipaje = ("camisa", "pantalón", "vestido", "calcetines", "zapatos", "mocacines", "corbata")
+mensaje = 'El índice es: '
+print(mensaje, equipaje[0])
+print(mensaje, equipaje[1])
+print(mensaje, equipaje[2])
+print(mensaje, equipaje[3])
+print(mensaje, equipaje[4])
+print(mensaje, equipaje[0])
+print(mensaje, equipaje[5])
+print(mensaje, equipaje[6])
